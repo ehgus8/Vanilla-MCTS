c7b11507254f6bdf8ec8dfe9e54840d3f8b97261
ehgus8 <ehgus8@kyonggi.ac.kr>
2023-08-15 15:45:45 +0900

    add codes

--- mcts.py
+++ mcts.py
@@ -1,16 +1,33 @@
 class MCTS:
     
-    def __init__(self) -> None:
-        pass
+    def __init__(self, game) -> None:
+        self.game = game
 
-    def forward():
-        pass
+    def forward(self, root):
+        trace = [root]
+        node = root
+        while not node.is_terminal():
+            node = self.select(node)
+            trace.append(node)
+        
+        self.expand(node)
+        score = self.simulation(node)
+        self.backup(trace, score)
 
-    def select():
-        pass
+        return root
     
-    def expand():
-        pass
+    def select(self, node):
+        return node.select()
     
-    def backup():
-        pass
+    def expand(self, node):
+        actions = None
+        node.expand(actions)
+    
+    def simulation(self, node):
+        return self.game.simulation(node)
+
+    def backup(self, trace, score):
+        for node in trace[::-1]:
+            node.visit += 1
+            node.score += score
+            score = score * -1
--- node.py
+++ node.py
@@ -1,4 +1,43 @@
+import math
+
 class Node:
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self, parent) -> None:
+        self.parent = parent # Node
+        self.children = None # List
+        self.state = None
+        self.to_player = None # player to act in this state, to make next state.
+        self.action = None # index or coordinate of actions to become this state.
+        self.visit = 0
+        self.score = 0
+        self.is_game_ended = False
+
+    def is_terminal(self):
+        return self.is_game_ended or self.children == None
+
+    def select(self):
+        max_uct = -999
+        max_idx = 0
+        children = self.children
+        for idx, child in enumerate(children):
+            uct = child.score + math.sqrt(2)*math.sqrt(math.log(self.visit)/child.visit)
+            if uct > max_uct:
+                max_uct = uct
+                max_idx = idx
+        
+        return children[max_idx]
+
+    def expand(self, actions):
+        if actions == None:
+            self.is_game_ended = True
+
+        children = []
+        for action in actions:
+            child = Node(self)
+            child.action = action
+
+        self.children = children
+
+        return None
+    
+
