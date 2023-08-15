b6d636121bb870a072b292c24d6746e2fa6fedf8
ehgus8 <ehgus8@kyonggi.ac.kr>
2023-08-15 17:40:06 +0900

    fix select, forward, backup function

--- mcts.py
+++ mcts.py
@@ -3,21 +3,34 @@ class MCTS:
     def __init__(self, game) -> None:
         self.game = game
 
+    # forward : selection -> expansion -> simulation -> backpropagation
     def forward(self, root):
         trace = [root]
         node = root
+
         while not node.is_terminal():
             node = self.select(node)
             trace.append(node)
         
-        self.expand(node)
-        score = self.simulation(node)
-        self.backup(trace, score)
+        if node.is_game_ended:
+            self.backup(trace, node.winner)
+            return
 
-        return root
+        self.expand(node)
+        score = self.simulation(node) # score is the number of winning player or draw 0
+        self.backup(trace, score)
     
     def select(self, node):
-        return node.select()
+        node = node.select()
+        game = self.game
+        state, score, done = game.step(node.parent.state, node.action, to_player=node.parent.to_player)
+        node.state = state
+
+        if done:
+            node.winner = score
+            node.is_game_ended = True
+
+        return node
     
     def expand(self, node):
         actions = self.game.get_valid_action_list(node.state.flatten(), shuffle=True)
@@ -30,4 +43,4 @@ class MCTS:
         for node in trace[::-1]:
             node.visit += 1
             node.score += score
-            score = score * -1
+            # score = score * -1
