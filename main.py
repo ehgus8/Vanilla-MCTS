3154f04aaafa9f26121c8b9748aaf85ae21ba238
ehgus8 <68594605+ehgus8@users.noreply.github.com>
2025-02-15 16:02:57 +0900

    feat: add game selection feature

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from tictactoe import TicTacToe
 from connect4 import Connect4
 
-# game = TicTacToe()
-game = Connect4()
-
-game.play_against_mcts(mcts_iterations = 500)
+if __name__ == '__main__':
+    while True:
+        print('1. TicTacToe')
+        print('2. Connect4')
+        num = int(input())
+        if num == 1:
+            game = TicTacToe()
+        elif num == 2:
+            game = Connect4()
+        
+        game.play_against_mcts(mcts_iterations = 500)
