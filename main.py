600ccad6a88c93d8b6fd95545d6308d028de32d9
ldh <68594605+ehgus8@users.noreply.github.com>
2025-03-01 11:45:44 +0900

    feat: add the game of gomoku

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from tictactoe import TicTacToe
 from connect4 import Connect4
+from gomoku import Gomoku
 
 if __name__ == '__main__':
     while True:
         print('1. TicTacToe')
         print('2. Connect4')
+        print('3. Gomoku')
         num = int(input())
         if num == 1:
             game = TicTacToe()
         elif num == 2:
             game = Connect4()
+        elif num == 3:
+            game = Gomoku()
         
-        game.play_against_mcts(mcts_iterations = 500)
+        game.play_against_mcts(mcts_iterations = 100)
